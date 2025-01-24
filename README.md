d035615f337d1beb018ea365b88fc0128c7f3563
Johanatan Cristhian Farfan <farfan463@gmail.com>
2025-01-24 17:57:29 -0500

    agregar funcionalidad js y readme

---
+++ README.md
@@ -0,0 +1,24 @@
+# Reto Amigo Secreto
+
+Este proyecto es una aplicación para organizar un juego de amigo secreto.
+
+## Tecnologías Utilizadas
+
+- HTML
+- CSS
+- JavaScript
+
+## Funciones y Validaciones
+
+- Registro de nombre de amigo.
+- Validación de nombre de amigo
+- Sorteo aleatorio de amigo secreto.
+- Validación para evitar duplicados en la lista de amigos.
+
+## Ejecución
+
+Para ejecutar la aplicación, simplemente abre el archivo `index.html` en tu navegador web.
+
+## Licencia
+
+Este proyecto es un challenge de Alura Latam junto a Oracle.
--- app.js
+++ app.js
@@ -1,1 +1,77 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+let inputAmigo = getElemento("amigo");
+let listaAmigos = getElemento("listaAmigos");
+let listaResultado = getElemento("resultado");
+let indiceAleatorio = -1;
+
+function getElemento(id) {
+  return document.getElementById(id);
+}
+
+function agregarAmigo() {
+  let nombreValido = validarNombre(inputAmigo.value);
+  if (!nombreValido) {
+    alert("Por favor, inserte un nombre.");
+    limpiarInput();
+    return;
+  }
+  if (listaResultado.childElementCount > 0) limpiarResultado();
+  if (!validarAmigoDuplicado(inputAmigo.value)) {
+    alert("El amigo ya se encuentra en la lista.");
+    limpiarInput();
+  }
+  amigos.push(inputAmigo.value);
+  actualizarListaAmigos();
+  actualizarIndiceAleatorio();
+  limpiarInput();
+}
+
+function validarNombre(nombre) {
+  const REGEX = /^[a-zA-Z\s]+$/;
+  let nombreValido = REGEX.test(nombre);
+  return nombre === "" || !nombreValido ? false : true;
+}
+function validarAmigoDuplicado(nombre) {
+  return amigos.some((amigo) => amigo.toLowerCase() === nombre.toLowerCase())
+    ? false
+    : true;
+}
+function actualizarListaAmigos() {
+  listaAmigos.innerHTML = "";
+  for (let i = 0; i < amigos.length; i++) {
+    let nuevoItemLista = document.createElement("li");
+    nuevoItemLista.innerHTML = amigos[i];
+    listaAmigos.appendChild(nuevoItemLista);
+  }
+}
+function actualizarIndiceAleatorio() {
+  indiceAleatorio = Math.floor(Math.random() * amigos.length);
+  console.log(indiceAleatorio);
+}
+
+function limpiarInput() {
+  inputAmigo.value = "";
+  inputAmigo.focus();
+}
+function limpiarListaAmigos() {
+  amigos = [];
+  listaAmigos.innerHTML = "";
+}
+function limpiarResultado() {
+  listaResultado.innerHTML = "";
+}
+function sortearAmigo() {
+  if (amigos.length === 0) {
+    alert("Lista de amigos vacia.");
+    return;
+  }
+  if (amigos.length === 1) {
+    alert("No hay suficientes amigos para sortear.");
+    return;
+  }
+  let amigoSorteado = amigos[indiceAleatorio];
+  let nuevoItemResultado = document.createElement("li");
+  nuevoItemResultado.innerHTML = `El amigo secreto sorteado es: ${amigoSorteado}`;
+  listaResultado.appendChild(nuevoItemResultado);
+  limpiarListaAmigos();
+}
